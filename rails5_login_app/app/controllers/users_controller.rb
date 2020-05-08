b6f25b1078acb566c109418e8edb2f465204a4e2
n.miyaho@gmail.com <n.miyaho@gmail.com>
2020-05-08 10:11:52 +0900

    2020050706 correct

--- rails5_login_app/app/controllers/users_controller.rb
+++ rails5_login_app/app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+  # def show
+  #   @items = Item.where(user_id: current_user.id)
+  #   編集後   
+  # end
   def show
-    # @items = Item.where(user_id: current_user.id)
-    # 編集後
-     @user = User.find(current_user.id)
+     @user = User.find(current_user.id)     
   end
-
 end
